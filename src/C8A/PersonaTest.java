e7dd857fb995b32bfc0bca0b405fdbd988046a14
Daniel Romero <dsromeror@unal.edu.co>
2021-08-14 14:45:58 -0500

    [ADD] Se agrega actividad de JUnit

--- src/C8A/PersonaTest.java
+++ src/C8A/PersonaTest.java
@@ -1,4 +1,26 @@
+package C8A;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonaTest {
-  
+
+    @Test
+    public void nombreCompleto() {
+        Persona persona = new Persona("Daniel","Romero","dsromeror@unal.edu.co",25);
+        String nombreCompleto = persona.nombreCompleto();
+
+        assertEquals("Daniel Romero",nombreCompleto);
+
+    }
+
+    @Test
+    public void esMayor() {
+        Persona persona = new Persona("Daniel","Romero","dsromeror@unal.edu.co",5);
+        boolean esMayor = persona.esMayor();
+
+        assertTrue(esMayor);
+
+    }
 }
--- src/C8A/Taza.java
+++ src/C8A/Taza.java
@@ -1,2 +1,22 @@
-package C8A;public class Taza {
+package C8A;
+
+public class Taza {
+    private String tipoLiquido;
+    private double porcentaje;
+    public Taza(String tipoLiquido, double porcentaje) {
+        this.tipoLiquido = tipoLiquido;
+        this.porcentaje = porcentaje;
+    }
+    public String getTipoLiquido() {
+        return tipoLiquido;
+    }
+    public void setTipoLiquido(String tipoLiquido){
+       this.tipoLiquido = tipoLiquido;
+    }
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
 }
